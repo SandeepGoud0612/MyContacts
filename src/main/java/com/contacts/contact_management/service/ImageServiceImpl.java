ca2dcec3d9fd59f8fe311e1f4b8ff7b4d7a727cb
JavaTraining <JavaTraining@DESKTOP-9350SMM.fios-router.home>
2017-02-17 20:11:42 -0500

    Added log statements in ImageServiceImpl Class.

--- src/main/java/com/contacts/contact_management/service/ImageServiceImpl.java
+++ src/main/java/com/contacts/contact_management/service/ImageServiceImpl.java
@@ -7,11 +7,13 @@ import java.util.List;
 
 import javax.transaction.Transactional;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.contacts.contact_management.dao.ImageDAO;
 import com.contacts.contact_management.model.Image;
+import com.contacts.utils.MyLogger;
 
 /**
  * @author JavaTraining
@@ -21,36 +23,38 @@ import com.contacts.contact_management.model.Image;
 @Transactional
 public class ImageServiceImpl implements ImageService {
 
+	private Logger log = MyLogger.getLogger();
+
 	@Autowired
 	private ImageDAO imageDAO;
 
 	@Override
 	public Image getImageById(final Long id) {
-		// TODO Auto-generated method stub
+		log.debug("In getImageById method.");
 		return imageDAO.findOne(id);
 	}
 
 	@Override
 	public List<Image> getAllImages() {
-		// TODO Auto-generated method stub
+		log.debug("In getAllImages method.");
 		return imageDAO.findAll();
 	}
 
 	@Override
 	public Image createImage(final Image image) {
-		// TODO Auto-generated method stub
+		log.debug("In createImage method");
 		return imageDAO.save(image);
 	}
 
 	@Override
 	public Image updateImage(final Image image) {
-		// TODO Auto-generated method stub
+		log.debug("In updateImage method");
 		return imageDAO.save(image);
 	}
 
 	@Override
 	public void deleteImage(final Long id) {
-		// TODO Auto-generated method stub
+		log.debug("In deleteImage method");
 		imageDAO.delete(id);
 	}
 
