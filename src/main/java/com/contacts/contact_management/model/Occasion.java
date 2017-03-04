9bd358f9ecd5d76384c7937030a7e33bd2984b22
Sandeep <sandeepgoud0612@gmail.com>
2017-03-04 17:18:07 -0500

    Signed-off-by: Sandeep <sandeepgoud0612@gmail.com>

--- src/main/java/com/contacts/contact_management/model/Occasion.java
+++ src/main/java/com/contacts/contact_management/model/Occasion.java
@@ -10,6 +10,8 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -33,6 +35,7 @@ public class Occasion extends BaseEntity implements Serializable {
 
 	@Column(name = "OCCASION_DATE", nullable = false)
 	@NotNull
+	@Temporal(TemporalType.DATE)
 	private Date date;
 
 	@Column(name = "REMINDME", nullable = false)
