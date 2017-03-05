44f971b26ccef877bdcb79440d6b2834e6abdaf8
Sandeep <sandeepgoud0612@gmail.com>
2017-03-04 21:41:24 -0500

    Signed-off-by: Sandeep <sandeepgoud0612@gmail.com>

--- src/main/java/com/contacts/utils/CopyObjects.java
+++ src/main/java/com/contacts/utils/CopyObjects.java
@@ -3,6 +3,8 @@ package com.contacts.utils;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.util.CollectionUtils;
+
 import com.contacts.contact_management.model.Address;
 import com.contacts.contact_management.model.Image;
 import com.contacts.contact_management.model.Occasion;
@@ -10,42 +12,72 @@ import com.contacts.contact_management.model.Person;
 
 public class CopyObjects {
 
-	public static Person copyPerson(final Person sourcePerson, final Person destinationPerson) {
-		destinationPerson.setFirstName(sourcePerson.getFirstName());
-		destinationPerson.setLastName(sourcePerson.getLastName());
-		destinationPerson.setDob(sourcePerson.getDob());
-		destinationPerson.setGender(sourcePerson.getGender());
-		destinationPerson.setEmailId(sourcePerson.getEmailId());
-		destinationPerson.setAlternateEmailId(sourcePerson.getAlternateEmailId());
-		destinationPerson.setPhoneNumber(sourcePerson.getPhoneNumber());
-		destinationPerson.setAlternatePhoneNumber(sourcePerson.getAlternatePhoneNumber());
-		for (int i = 0; i < destinationPerson.getAddressList().size(); i++) {
-			Address sourceAddress = sourcePerson.getAddressList().get(i);
-			Address destinationAddress = getDestinationAddress(sourceAddress, destinationPerson.getAddressList());
+	public static Person copyPerson(final Person uiPerson, final Person dbPerson) {
+		dbPerson.setFirstName(uiPerson.getFirstName());
+		dbPerson.setLastName(uiPerson.getLastName());
+		dbPerson.setDob(uiPerson.getDob());
+		dbPerson.setGender(uiPerson.getGender());
+		dbPerson.setEmailId(uiPerson.getEmailId());
+		dbPerson.setAlternateEmailId(uiPerson.getAlternateEmailId());
+		dbPerson.setPhoneNumber(uiPerson.getPhoneNumber());
+		dbPerson.setAlternatePhoneNumber(uiPerson.getAlternatePhoneNumber());
+		for (int i = 0; i < dbPerson.getAddressList().size(); i++) {
+			Address sourceAddress = uiPerson.getAddressList().get(i);
+			Address destinationAddress = getDestinationAddress(sourceAddress, dbPerson.getAddressList());
 			copyAddress(sourceAddress, destinationAddress);
 		}
-		for (int i = 0; i < destinationPerson.getOccasionList().size(); i++) {
-			Occasion destinationOccasion = destinationPerson.getOccasionList().get(i);
-			Occasion sourceOccasion = getSourceOccasion(destinationOccasion, sourcePerson.getOccasionList());
-			if(sourceOccasion == null){
-				destinationPerson.getOccasionList().set(i, null);
-				continue;
+
+		if (!CollectionUtils.isEmpty(uiPerson.getOccasionList())) {
+			if (uiPerson.getOccasionList().size() == dbPerson.getOccasionList().size()) {
+				updateOccationList(uiPerson.getOccasionList(), dbPerson.getOccasionList());
+			} else if (uiPerson.getOccasionList().size() > dbPerson.getOccasionList().size()) {
+				addOccasionInOccasionList(uiPerson.getOccasionList(), dbPerson.getOccasionList(), dbPerson);
+			} else {
+				deleteOccasionFromOccasionList(uiPerson.getOccasionList(), dbPerson.getOccasionList());
 			}
-			copyOccasion(sourceOccasion, destinationOccasion);
 		}
 		// copyImage(destinationPerson.getImage(),sourcePerson.getImage());
-		return destinationPerson;
+		return dbPerson;
+	}
+
+	private static void updateOccationList(List<Occasion> uiOccasionList, List<Occasion> dbOccasionList) {
+		for (int i = 0; i < uiOccasionList.size(); i++) {
+			Occasion uiOccasion = uiOccasionList.get(i);
+			Occasion dbOccasion = dbOccasionList.stream()
+					.filter(occasion -> occasion.getId().equals(uiOccasion.getId())).collect(Collectors.toList())
+					.get(0);
+			copyOccasion(uiOccasion, dbOccasion);
+		}
+	}
+
+	private static void addOccasionInOccasionList(List<Occasion> uiOccasionList, List<Occasion> dbOccasionList, Person dbPerson) {
+		for (int i = 0; i < uiOccasionList.size(); i++) {
+			Occasion uiOccasion = uiOccasionList.get(i);
+			List<Occasion> dbOccasions = dbOccasionList.stream()
+					.filter(occasion -> occasion.getId().equals(uiOccasion.getId())).collect(Collectors.toList());
+			if (CollectionUtils.isEmpty(dbOccasions)) {
+				uiOccasion.setPerson(dbPerson);
+				dbOccasionList.add(uiOccasion);
+				break;
+			}
+		}
+	}
+
+	private static void deleteOccasionFromOccasionList(List<Occasion> uiOccasionList, List<Occasion> dbOccasionList) {
+		for (int i = 0; i < dbOccasionList.size(); i++) {
+			Occasion dbOccasion = dbOccasionList.get(i);
+			List<Occasion> uiOccasions = uiOccasionList.stream()
+					.filter(occasion -> occasion.getId().equals(dbOccasion.getId())).collect(Collectors.toList());
+			if (CollectionUtils.isEmpty(uiOccasions)) {
+				dbOccasionList.set(i, null);
+				break;
+			}
+		}
 	}
 
 	private static Address getDestinationAddress(Address sourceAddress, List<Address> destinationAddressList) {
 		return destinationAddressList.stream().filter(address -> address.getId().equals(sourceAddress.getId()))
 				.collect(Collectors.toList()).get(0);
-	}
-
-	private static Occasion getSourceOccasion(Occasion destinationOccasion, List<Occasion> sourceOccasionList) {
-		 List<Occasion> occasions = sourceOccasionList.stream().filter(occasion -> occasion.getId().equals(destinationOccasion.getId()))
-				.collect(Collectors.toList());
-		 return occasions.size() > 0 ? occasions.get(0) : null; 
 	}
 
 	public static Address copyAddress(final Address sourceAddress, final Address destinationAddress) {
