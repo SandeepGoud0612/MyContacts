f676c9647b3d2691175892830c969a37de7c9eff
Sandeep <sandeepgoud0612@gmail.com>
2017-03-04 19:03:55 -0500

    Signed-off-by: Sandeep <sandeepgoud0612@gmail.com>

--- src/main/java/com/contacts/utils/CopyObjects.java
+++ src/main/java/com/contacts/utils/CopyObjects.java
@@ -1,13 +1,16 @@
 package com.contacts.utils;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import com.contacts.contact_management.model.Address;
 import com.contacts.contact_management.model.Image;
 import com.contacts.contact_management.model.Occasion;
 import com.contacts.contact_management.model.Person;
 
 public class CopyObjects {
-	
-	public static Person copyPerson(final Person sourcePerson, final Person destinationPerson){
+
+	public static Person copyPerson(final Person sourcePerson, final Person destinationPerson) {
 		destinationPerson.setFirstName(sourcePerson.getFirstName());
 		destinationPerson.setLastName(sourcePerson.getLastName());
 		destinationPerson.setDob(sourcePerson.getDob());
@@ -16,17 +19,36 @@ public class CopyObjects {
 		destinationPerson.setAlternateEmailId(sourcePerson.getAlternateEmailId());
 		destinationPerson.setPhoneNumber(sourcePerson.getPhoneNumber());
 		destinationPerson.setAlternatePhoneNumber(sourcePerson.getAlternatePhoneNumber());
-		for(int i = 0; i < destinationPerson.getAddressList().size(); i++){
-			copyAddress(sourcePerson.getAddressList().get(i), destinationPerson.getAddressList().get(i));
+		for (int i = 0; i < destinationPerson.getAddressList().size(); i++) {
+			Address sourceAddress = sourcePerson.getAddressList().get(i);
+			Address destinationAddress = getDestinationAddress(sourceAddress, destinationPerson.getAddressList());
+			copyAddress(sourceAddress, destinationAddress);
 		}
-		for(int i = 0; i < destinationPerson.getOccasionList().size(); i++){
-			copyOccasion(sourcePerson.getOccasionList().get(i), destinationPerson.getOccasionList().get(i)	);
+		for (int i = 0; i < destinationPerson.getOccasionList().size(); i++) {
+			Occasion destinationOccasion = destinationPerson.getOccasionList().get(i);
+			Occasion sourceOccasion = getSourceOccasion(destinationOccasion, sourcePerson.getOccasionList());
+			if(sourceOccasion == null){
+				destinationPerson.getOccasionList().set(i, null);
+				continue;
+			}
+			copyOccasion(sourceOccasion, destinationOccasion);
 		}
-		//copyImage(destinationPerson.getImage(),sourcePerson.getImage());
+		// copyImage(destinationPerson.getImage(),sourcePerson.getImage());
 		return destinationPerson;
 	}
-	
-	public static Address copyAddress(final Address sourceAddress, final Address destinationAddress){
+
+	private static Address getDestinationAddress(Address sourceAddress, List<Address> destinationAddressList) {
+		return destinationAddressList.stream().filter(address -> address.getId().equals(sourceAddress.getId()))
+				.collect(Collectors.toList()).get(0);
+	}
+
+	private static Occasion getSourceOccasion(Occasion destinationOccasion, List<Occasion> sourceOccasionList) {
+		 List<Occasion> occasions = sourceOccasionList.stream().filter(occasion -> occasion.getId().equals(destinationOccasion.getId()))
+				.collect(Collectors.toList());
+		 return occasions.size() > 0 ? occasions.get(0) : null; 
+	}
+
+	public static Address copyAddress(final Address sourceAddress, final Address destinationAddress) {
 		destinationAddress.setAddressLine1(sourceAddress.getAddressLine1());
 		destinationAddress.setAddressLine2(sourceAddress.getAddressLine2());
 		destinationAddress.setCity(sourceAddress.getCity());
@@ -35,18 +57,18 @@ public class CopyObjects {
 		destinationAddress.setZipCode(sourceAddress.getZipCode());
 		return destinationAddress;
 	}
-	
-	public static Occasion copyOccasion(final Occasion sourceOccasion, final Occasion destinationOccasion){
+
+	public static Occasion copyOccasion(final Occasion sourceOccasion, final Occasion destinationOccasion) {
 		destinationOccasion.setName(sourceOccasion.getName());
 		destinationOccasion.setDate(sourceOccasion.getDate());
 		destinationOccasion.setRemindMe(sourceOccasion.getRemindMe());
 		return destinationOccasion;
-	}	
-	
-	public static Image copyImage(final Image sourceImage, final Image destinationImage){
+	}
+
+	public static Image copyImage(final Image sourceImage, final Image destinationImage) {
 		destinationImage.setImage(sourceImage.getImage());
 		destinationImage.setTag(sourceImage.getTag());
 		return destinationImage;
 	}
-	
+
 }
