8ccd2758339765f6acbd37ee388369e92789f3d3
Sandeep <sandeepgoud0612@gmail.com>
2017-03-12 20:02:22 -0400

    Signed-off-by: Sandeep <sandeepgoud0612@gmail.com>

--- src/main/java/com/contacts/MyContactsApplication.java
+++ src/main/java/com/contacts/MyContactsApplication.java
@@ -3,13 +3,20 @@ package com.contacts;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 @EnableScheduling
-public class MyContactsApplication {
+public class MyContactsApplication extends WebMvcConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyContactsApplication.class, args);
 	}
-	
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/index").setViewName("index");
+	}
+
 }
--- src/main/java/com/contacts/notification_management/service/EmilService.java
+++ src/main/java/com/contacts/notification_management/service/EmilService.java
@@ -5,17 +5,41 @@ import java.util.Date;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
+@Component
 public class EmilService {
 	
 	private static final Logger log = LoggerFactory.getLogger(EmilService.class);
 
 	private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 	
-	@Scheduled(fixedRate = 10000)
+	@Scheduled(fixedRate = 5000)
 	public void invoke() {
 		log.info("The time is now {}", dateFormat.format(new Date()));
+		//prepareAndSend();
+	}
+	
+	private JavaMailSender mailSender;
+	 
+    @Autowired
+    public EmilService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+	
+	public void prepareAndSend() {
+		SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("foo@example.com");
+        message.setTo("sandeepgoud0612@gmail.com");
+        message.setSubject("hello");
+        mailSender.send(message);
 	}
 
 
